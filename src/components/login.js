4b65a04c6d22104d06590a735c76bb8080f1d48c
amboob223 <monk8896@gmail.com>
2024-02-16 05:00:08 -0500

    ico

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,13 @@
-import {React} from "react";
+import React from "react";
 import "../App.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
-  
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
-      
+
     const loginEmail = document.getElementById("loginEmail").value;
     const loginPassword = document.getElementById("loginPassword").value;
 
@@ -21,9 +21,14 @@ function Login() {
 
     if (response.status === 200) {
       console.log("Login successful:", result.message);
-        window.location.href = "/owner" 
       document.getElementById("messag").innerText = "Login successful";
-      // Redirect or perform any other action after successful login
+
+      // Clear input fields
+      document.getElementById("loginEmail").value = "";
+      document.getElementById("loginPassword").value = "";
+
+      // Use navigate to navigate to the '/owner' route
+      navigate('/owner');
     } else {
       console.log("Login failed:", result.error);
       document.getElementById("messag").innerText = "Login failed, try again";
@@ -32,38 +37,34 @@ function Login() {
 
   return (
     <div>
-       <div style={{textAlign:"center",display:"flex",fontSize:"20px", justifyContent:"space-between", marginTop:"3%", marginBottom:"1%", backgroundColor:"lightgrey"}}>
+      <div style={{ textAlign: "center", display: "flex", fontSize: "20px", justifyContent: "space-between", marginTop: "3%", marginBottom: "1%", backgroundColor: "lightgrey" }}>
 
-     <div style={{textAlign:"center", justifyContent:"center"}}>
-      
-      <form className="login" id="login-form" onSubmit={handleLogin} style={{display:"flex"}}>
-        <label htmlFor="loginEmail">Email</label>
-        <input type="email" id="loginEmail" name="loginEmail" required />
-        <br />
-        <label htmlFor="loginPassword">Password</label>
-        <input type="password" id="loginPassword" name="loginPassword" required />
-        <br />
-        <button className="btn btn-warning" type="submit">Login</button>
-      </form>
-        <p id="messag"></p>
-    </div>
+        <div style={{ textAlign: "center", justifyContent: "center" }}>
 
-<div>
-        <p>
+          <form className="login" id="login-form" onSubmit={handleLogin} style={{ display: "flex" }}>
+            <label htmlFor="loginEmail">Email</label>
+            <input type="email" id="loginEmail" name="loginEmail" required />
+            <br />
+            <label htmlFor="loginPassword">Password</label>
+            <input type="password" id="loginPassword" name="loginPassword" required />
+            <br />
+            <button className="btn btn-warning" type="submit">Login</button>
+          </form>
+          <p id="messag"></p>
+        </div>
+
+        <div>
+          <p>
             Are you a shop Owner?
-        </p>
+          </p>
           <Link to="/sign">
-        Sign up
-    </Link>
-     </div>
-  
-     </div>
+            Sign up
+          </Link>
+        </div>
 
-     
+      </div>
 
     </div>
-   
-     
   );
 }
 
